2a004be8e48c275c00e4941682e31984bd253f27
zaki82000 <yabdelshafy0@gmail.com>
2024-04-22 17:52:49 +0200

    add sups python file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,17 @@
-#!/usr/bin/python3
 import requests
 
 def number_of_subscribers(subreddit):
+    """
+    Retrieves the number of subscribers for a given subreddit.
+
+    Parameters:
+    - subreddit (str): The name of the subreddit.
+
+    Returns:
+    - int: The number of subscribers for the subreddit. Returns 0 if the subreddit is invalid.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'YourBotName'}  # Set a custom User-Agent
+    headers = {'User-Agent': '0x16-api_advanced:project: v1.0.0'}  # Set a custom User-Agent
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
